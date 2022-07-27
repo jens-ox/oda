22a6af9f3b8fa4bd3017af69e33dee25a7a6a0dd
Jens Ochsenmeier <github@jens-ox.de>
2022-07-27 10:45:20 +0200

    set up cron

---
+++ crawl/exporters/index.ts
@@ -0,0 +1,10 @@
+import BafinExporter from './bafin'
+
+const exporters = [
+  {
+    sourceId: 'bafin-stimmrechte',
+    exporter: BafinExporter
+  }
+]
+
+export default exporters
--- crawl/index.ts
+++ crawl/index.ts
@@ -3,30 +3,36 @@ import { createHash } from 'crypto'
 import prisma from '../lib/prisma'
 import { upload } from '../lib/aws'
 import { compress } from '../lib/compress'
-import BafinExporter from './exporters/bafin'
+import exporters from './exporters'
 
-const main = async () => {
-  const data = await BafinExporter()
-  const dataString = await compress(data)
+const main = async () =>
+  Promise.all(
+    exporters.map(async (e) => {
+      const data = await e.exporter()
+      const dataString = await compress(data)
 
-  // stringify and md5
-  const dataMd5 = createHash('md5').update(dataString).digest('hex')
+      // stringify and md5
+      const dataMd5 = createHash('md5').update(dataString).digest('hex')
 
-  // check if md5 matches current version -- if not, insert
-  const currentEntry = await prisma.snapshot.findFirst({ orderBy: [{ createdAt: 'desc' }] })
-  if (currentEntry === null || currentEntry.md5 !== dataMd5) {
-    // upload to AWS
-    await upload(dataMd5, dataString)
+      // check if md5 matches current version -- if not, insert
+      const currentEntry = await prisma.snapshot.findFirst({
+        where: { sourceId: e.sourceId },
+        orderBy: [{ createdAt: 'desc' }]
+      })
+      if (currentEntry === null || currentEntry.md5 !== dataMd5) {
+        // upload to AWS
+        await upload(dataMd5, dataString)
 
-    // insert into db
-    await prisma.snapshot.create({
-      data: {
-        md5: dataMd5,
-        createdAt: new Date(),
-        sourceId: 'bafin-stimmrechte'
+        // insert into db
+        await prisma.snapshot.create({
+          data: {
+            md5: dataMd5,
+            createdAt: new Date(),
+            sourceId: e.sourceId
+          }
+        })
       }
     })
-  }
-}
+  )
 
 main()
