c90559efd464566347f94131a3543d791046ea63
Jens Ochsenmeier <github@jens-ox.de>
2022-10-18 14:10:19 +0200

    typical iso date bug

--- utils/germanDateToString.ts
+++ utils/germanDateToString.ts
@@ -1,6 +1,6 @@
 import { formatISO, parse } from 'date-fns'
 
 const germanDateToString = (dateString?: string): string | undefined =>
-  dateString ? formatISO(parse(dateString, 'dd.MM.yyyy', new Date()), { format: 'basic' }) : undefined
+  dateString ? formatISO(parse(dateString, 'dd.MM.yyyy', new Date()), { representation: 'date' }) : undefined
 
 export default germanDateToString
