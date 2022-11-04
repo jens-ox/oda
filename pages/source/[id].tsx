090bcbb921adb495a1ca4ed888e09ac02d1b9f50
Jens Ochsenmeier <github@jens-ox.de>
2022-11-04 12:17:51 +0100

    wip: add JSON schemas to sources

--- pages/source/[id].tsx
+++ pages/source/[id].tsx
@@ -1,8 +1,12 @@
 import { Source } from '@prisma/client'
 import { GetServerSideProps, NextPage } from 'next'
+import classNames from 'classnames'
+import { Tab } from '@headlessui/react'
+import { PropsWithChildren } from 'react'
 import SnapshotsTable from '../../components/tables/Snapshots'
 import prisma from '../../lib/prisma'
 import { SerializedSnapshot } from '../../types'
+import { ebGaramond } from '../../utils/fonts'
 
 interface SourcePageProps {
   source: Source | null
@@ -43,6 +47,34 @@ export const getServerSideProps: GetServerSideProps<SourcePageProps> = async ({ 
   }
 }
 
+interface TabHeaderProps {
+  tabKey: string
+  label: string
+}
+const TabHeader: React.FC<TabHeaderProps> = ({ tabKey, label }) => (
+  <Tab
+    key={tabKey}
+    className={({ selected }) =>
+      classNames(
+        selected
+          ? 'border-indigo-500 text-indigo-600'
+          : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300',
+        'whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm outline-none'
+      )
+    }
+  >
+    {label}
+  </Tab>
+)
+
+interface TabPanelProps {
+  tabKey: string
+}
+
+const TabPanel: React.FC<PropsWithChildren<TabPanelProps>> = ({ tabKey, children }) => (
+  <Tab.Panel key={tabKey}>{children}</Tab.Panel>
+)
+
 const SourcePage: NextPage<SourcePageProps> = ({ source, snapshots }) => {
   return (
     <div>
@@ -51,7 +83,7 @@ const SourcePage: NextPage<SourcePageProps> = ({ source, snapshots }) => {
       ) : (
         <div className="flex flex-col gap-12">
           <div className="flex flex-col">
-            <h1 className="font-serif font-medium text-5xl mb-4">{source.name}</h1>
+            <h1 className={classNames(ebGaramond.className, 'font-medium text-5xl mb-4')}>{source.name}</h1>
             <small className="text-stone-500">{source.description}</small>
             <small className="text-stone-500">
               —{' '}
@@ -60,7 +92,25 @@ const SourcePage: NextPage<SourcePageProps> = ({ source, snapshots }) => {
               </a>
             </small>
           </div>
-          {snapshots === null ? <p>Keine Snapshots gefunden</p> : <SnapshotsTable snapshots={snapshots} />}
+
+          <div className="flex flex-col gap-6">
+            <Tab.Group>
+              <div className="border-b border-gray-200">
+                <Tab.List className="-mb-px flex space-x-8">
+                  <TabHeader tabKey="snapshots" label="Snapshots" />
+                  <TabHeader tabKey="schema" label="Schema" />
+                </Tab.List>
+              </div>
+              <Tab.Panels>
+                <TabPanel tabKey="snapshots">
+                  {snapshots === null ? <p>Keine Snapshots gefunden</p> : <SnapshotsTable snapshots={snapshots} />}
+                </TabPanel>
+                <TabPanel tabKey="schema">
+                  <p>Schema</p>
+                </TabPanel>
+              </Tab.Panels>
+            </Tab.Group>
+          </div>
         </div>
       )}
     </div>
