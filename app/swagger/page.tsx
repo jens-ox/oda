c9eac94162fe2a08c470e7d5f3437b966b176221
Jens Ochsenmeier <github@jens-ox.de>
2022-11-05 20:00:42 +0100

    fix prerendering in swagger page

--- app/swagger/page.tsx
+++ app/swagger/page.tsx
@@ -1,11 +1,9 @@
-'use client'
+import Swagger from '../../components/Swagger'
 
-import dynamic from 'next/dynamic'
-import Spec from '../../public/openapi.json'
-import 'swagger-ui-react/swagger-ui.css'
+const SwaggerPage = () => (
+  <div>
+    <Swagger />
+  </div>
+)
 
-const SwaggerUI = dynamic(import('swagger-ui-react'), { ssr: false })
-
-const Swagger = () => <SwaggerUI spec={Spec} />
-
-export default Swagger
+export default SwaggerPage
---
+++ components/Swagger.tsx
@@ -0,0 +1,8 @@
+'use client'
+import SwaggerUI from 'swagger-ui-react'
+import spec from '../public/openapi.json'
+import 'swagger-ui-react/swagger-ui.css'
+
+const Swagger: React.FC = () => <SwaggerUI spec={spec} />
+
+export default Swagger
