4ecdb1fd595c663c55d23b99da9db52f4edc82b9
Jens Ochsenmeier <github@jens-ox.de>
2022-12-31 22:51:37 +0100

    allow extractors to fail

--- crawl/index.ts
+++ crawl/index.ts
@@ -7,54 +7,58 @@ import exporterSources from '../sources/exporters'
 const main = async () =>
   Promise.all(
     exporterSources.map(async (s) => {
-      const result = await s.exporter.result()
-      const dataString = JSON.stringify(result)
-      const size = Math.floor(dataString.length / 1000)
+      try {
+        const result = await s.exporter.result()
+        const dataString = JSON.stringify(result)
+        const size = Math.floor(dataString.length / 1000)
 
-      // stringify and md5
-      const dataMd5 = createHash('md5').update(dataString).digest('hex')
+        // stringify and md5
+        const dataMd5 = createHash('md5').update(dataString).digest('hex')
 
-      // check if md5 matches most recent version -- if not, insert
-      const recentVersion = await prisma.snapshot.findFirst({
-        where: { sourceId: s.id },
-        orderBy: [{ createdAt: 'desc' }]
-      })
-
-      if (process.env.DRY_RUN) {
-        console.log(`dry-run: ${s.id}, hash ${dataMd5}, size ${size} kB`)
-      }
-
-      if (recentVersion === null || recentVersion.md5 !== dataMd5) {
-        // upload to AWS
-        await upload(dataMd5, dataString)
-
-        // if there is no recent version, no diff can be computed
-        let diff: unknown | null = null
-        let digest: unknown | null = null
-
-        // compute diff and digest
-        if (recentVersion !== null) {
-          const previousContent = await getData(recentVersion.md5)
-          diff = s.exporter.diff(previousContent as any, result as any)
-          digest = s.exporter.digest(diff as any)
-
-          // upload diff to db
-          await upload(`${dataMd5}-diff`, JSON.stringify(diff))
-        }
-
-        // insert into db
-        if (process.env.DRY_RUN) {
-          console.log(`[dry]: would create snapshot db entry (${dataMd5}, source ${s.id})`)
-        }
-        await prisma.snapshot.create({
-          data: {
-            md5: dataMd5,
-            createdAt: new Date(),
-            sourceId: s.id,
-            size,
-            digest: digest !== null ? JSON.stringify(digest) : ''
-          }
+        // check if md5 matches most recent version -- if not, insert
+        const recentVersion = await prisma.snapshot.findFirst({
+          where: { sourceId: s.id },
+          orderBy: [{ createdAt: 'desc' }]
         })
+
+        if (process.env.DRY_RUN) {
+          console.log(`dry-run: ${s.id}, hash ${dataMd5}, size ${size} kB`)
+        }
+
+        if (recentVersion === null || recentVersion.md5 !== dataMd5) {
+          // upload to AWS
+          await upload(dataMd5, dataString)
+
+          // if there is no recent version, no diff can be computed
+          let diff: unknown | null = null
+          let digest: unknown | null = null
+
+          // compute diff and digest
+          if (recentVersion !== null) {
+            const previousContent = await getData(recentVersion.md5)
+            diff = s.exporter.diff(previousContent as any, result as any)
+            digest = s.exporter.digest(diff as any)
+
+            // upload diff to db
+            await upload(`${dataMd5}-diff`, JSON.stringify(diff))
+          }
+
+          // insert into db
+          if (process.env.DRY_RUN) {
+            console.log(`[dry]: would create snapshot db entry (${dataMd5}, source ${s.id})`)
+          }
+          await prisma.snapshot.create({
+            data: {
+              md5: dataMd5,
+              createdAt: new Date(),
+              sourceId: s.id,
+              size,
+              digest: digest !== null ? JSON.stringify(digest) : ''
+            }
+          })
+        }
+      } catch (error) {
+        console.error(error)
       }
     })
   )
