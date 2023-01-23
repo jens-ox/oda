bf8c26e94b0632ae085a76d5907b11d6a089e41c
Jens Ochsenmeier <github@jens-ox.de>
2023-01-23 20:13:35 +0100

    add sources overview

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-const Home = async () => {
-  return (
-    <div className="flex flex-col gap-16">
-      <header>
-        <h1 className="font-serif text-5xl">Bundesdatenkrake</h1>
-        <small className="text-stone-500">
-          Extraktion, Versionierung und maschinenlesbare Bereitstellung öffentlicher Daten.
-        </small>
-      </header>
+import { SourceList } from '../components/SourceList'
 
-      <main>goo</main>
-    </div>
-  )
-}
+const Home = async () => (
+  <div className="flex flex-col gap-16">
+    <header>
+      <h1 className="font-serif text-5xl">Bundesdatenkrake</h1>
+      <small className="text-stone-500">
+        Extraktion, Versionierung und maschinenlesbare Bereitstellung öffentlicher Daten.
+      </small>
+    </header>
+
+    <main className="flex flex-col gap-6">
+      <SourceList />
+    </main>
+  </div>
+)
 
 export default Home
---
+++ components/SourceList.tsx
@@ -0,0 +1,45 @@
+'use client'
+
+import Link from 'next/link'
+import { useMemo, useState } from 'react'
+import { sources } from '../sources'
+
+export const SourceList: React.FC = () => {
+  const [search, setSearch] = useState('')
+
+  const filteredSources = useMemo(
+    () => sources.filter((s) => s.name.toLowerCase().includes(search.toLowerCase().trim())),
+    [sources, search]
+  )
+
+  return (
+    <>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Datenquellen durchsuchen"
+        className="rounded bg-white px-4 py-2 border border-slate-200 focus:border-slate-400 outline-none transition duration-200"
+      />
+      {filteredSources.length > 0 ? (
+        <div className="grid grid-cols-3 gap-4">
+          {filteredSources.map((source) => (
+            <Link href={`/source/${source.id}`}>
+              <div className="rounded-lg border border-slate-400/25 hover:border-slate-400/50 bg-white p-2">
+                <div className="flex flex-col gap-4 justify-between">
+                  <div>
+                    <h3 className="text-lg font-medium">{source.name}</h3>
+                    <p className="text-sm text-slate-600">{source.description}</p>
+                  </div>
+                  <div className="text-sm text-slate-600">{source.sourceName}</div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      ) : (
+        <p className="text-slate-600 italic">Keine passenden Datenquellen gefunden :(</p>
+      )}
+    </>
+  )
+}
