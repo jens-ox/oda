a050fad9c9b43a8cac23cf239596ecced879fa38
Jens Ochsenmeier <github@jens-ox.de>
2023-03-07 23:40:55 +0100

    dependency updates

--- utils/fonts.ts
+++ utils/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, EB_Garamond } from '@next/font/google'
+import { Inter, EB_Garamond } from 'next/font/google'
 
 export const inter = Inter({ display: 'optional', subsets: ['latin'], variable: '--font-inter' })
 export const ebGaramond = EB_Garamond({ display: 'optional', subsets: ['latin'], variable: '--font-garamond' })
