064133e8aaf456c4c9e098855555b25cde2337c2
Jens Ochsenmeier <github@jens-ox.de>
2023-03-14 12:54:26 +0100

    prepare surfacing of openapi file specs

--- app/api/sources/route.ts
+++ app/api/sources/route.ts
@@ -1,6 +1,15 @@
+import { generateSchema } from '@anatine/zod-openapi'
 import { NextResponse } from 'next/server'
 import { sources } from '../../../sources'
 
 export const GET = async () => {
-  return NextResponse.json(sources)
+  return NextResponse.json(
+    sources.map((s) => ({
+      ...s,
+      targets: Object.entries(s.targets ?? {}).reduce((acc, [key, value]) => {
+        acc[key] = generateSchema(value)
+        return acc
+      }, {} as Record<string, unknown>)
+    }))
+  )
 }
