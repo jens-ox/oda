98a4808e9adfd499a34a51df5f4be08faf4bb1b0
Jens Ochsenmeier <github@jens-ox.de>
2023-03-14 18:22:56 +0100

    no need to surface schemas in root route

--- app/api/sources/route.ts
+++ app/api/sources/route.ts
@@ -1,15 +1,6 @@
-import { generateSchema } from '@anatine/zod-openapi'
 import { NextResponse } from 'next/server'
 import { sources } from '../../../sources'
 
 export const GET = async () => {
-  return NextResponse.json(
-    sources.map((s) => ({
-      ...s,
-      targets: Object.entries(s.targets ?? {}).reduce((acc, [key, value]) => {
-        acc[key] = generateSchema(value)
-        return acc
-      }, {} as Record<string, unknown>)
-    }))
-  )
+  return NextResponse.json(sources.map(({ targets, ...s }) => s))
 }
