5952c4bb97f0caca734deca92ab3c172c5a319f4
Jens Ochsenmeier <github@jens-ox.de>
2023-12-18 14:24:22 +0100

    manual run, run crawler sequentially

--- crawl/index.ts
+++ crawl/index.ts
@@ -6,24 +6,22 @@ import { exporters } from '@/sources/exporters'
 const BASE_DIR = join(__dirname, '../data')
 
 ;(async () => {
-  await Promise.all(
-    exporters.map(async (s) => {
-      try {
-        const result = await s.exporter()
+  for (const s of exporters) {
+    try {
+      const result = await s.exporter()
 
-        // ensure that the target directory exists
-        await mkdir(join(BASE_DIR, s.id), { recursive: true })
+      // ensure that the target directory exists
+      await mkdir(join(BASE_DIR, s.id), { recursive: true })
 
-        // write files to disk
-        for (const file of result) {
-          const stringData = JSON.stringify(file.data, null, 2)
-          await writeFile(join(BASE_DIR, s.id, file.targetFile), stringData, {
-            encoding: 'utf-8'
-          })
-        }
-      } catch (error) {
-        console.error('error crawling: ', error)
+      // write files to disk
+      for (const file of result) {
+        const stringData = JSON.stringify(file.data, null, 2)
+        await writeFile(join(BASE_DIR, s.id, file.targetFile), stringData, {
+          encoding: 'utf-8'
+        })
       }
-    })
-  )
+    } catch (error) {
+      console.error('error crawling: ', error)
+    }
+  }
 })()
