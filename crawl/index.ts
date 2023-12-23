5b41be0a0831a52846706540b4b5a0f21861e479
Jens Ochsenmeier <github@jens-ox.de>
2023-12-23 11:19:51 +0100

    parallelize again

--- crawl/index.ts
+++ crawl/index.ts
@@ -6,22 +6,24 @@ import { exporters } from '@/sources/exporters'
 const BASE_DIR = join(__dirname, '../data')
 
 ;(async () => {
-  for (const s of exporters) {
-    try {
-      const result = await s.exporter()
+  await Promise.all(
+    exporters.map(async (s) => {
+      try {
+        const result = await s.exporter()
 
-      // ensure that the target directory exists
-      await mkdir(join(BASE_DIR, s.id), { recursive: true })
+        // ensure that the target directory exists
+        await mkdir(join(BASE_DIR, s.id), { recursive: true })
 
-      // write files to disk
-      for (const file of result) {
-        const stringData = JSON.stringify(file.data, null, 2)
-        await writeFile(join(BASE_DIR, s.id, file.targetFile), stringData, {
-          encoding: 'utf-8'
-        })
+        // write files to disk
+        for (const file of result) {
+          const stringData = JSON.stringify(file.data, null, 2)
+          await writeFile(join(BASE_DIR, s.id, file.targetFile), stringData, {
+            encoding: 'utf-8'
+          })
+        }
+      } catch (error) {
+        console.error('error crawling: ', error)
       }
-    } catch (error) {
-      console.error('error crawling: ', error)
-    }
-  }
+    })
+  )
 })()
